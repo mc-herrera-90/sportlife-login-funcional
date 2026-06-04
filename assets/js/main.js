bf30a9e2009793c122f020c3d98164d3c7d7b4de
marco <mcherrera>
2026-06-03 23:54:12 -0400

    datos cargados y lógica implementada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,53 @@
-// Eventos
+const formLogin = document.getElementById("login");
+const feedback = document.getElementById("feedback"); 
+
+window.addEventListener("load", async () => {
+
+    // Cargamos la data del JSON
+    const respuesta = await fetch("../../data/users.json");
+
+    const usuarios = respuesta.ok ? await respuesta.json() : [];
+
+    console.log(usuarios);
+
+    // Listener
+    formLogin.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const correo = event.target.elements.correo.value;
+        const password = event.target.elements.contrasenia.value;
+
+        // Buscar al usuario
+        const usuarioEncontrado = usuarios.find((usuario) => {
+            return usuario.user === correo.toLowerCase() && usuario.password === password.toLowerCase()
+        })
+
+        if (!usuarioEncontrado) {
+            feedback.classList.remove("d-none")
+            return;
+        }
+
+        // Guardamos al usuario encontrado en el localStorage
+        localStorage.setItem("sesionUsuario", JSON.stringify(usuarioEncontrado));
+
+        // Redirigir al dashboard según el rol
+        switch(usuarioEncontrado.role) {
+            case "user": 
+                window.location.href = "../dashboards/user.html";
+                break;
+
+            case "coach":
+                window.location.href = "../dashboards/coach.html"
+                break;
+            
+            case "admin":
+                window.location.href = "../dashboards/admin.html" 
+            
+            default:
+                feedback.classList.remove("d-none");
+                feedback.textContent = "Rol no encontrado";
+        }
+
+    })
+
+})
