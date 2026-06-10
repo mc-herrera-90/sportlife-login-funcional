8147f562d023c331a9ee75b5de0ecd7755ae3a07
mcherrera <mcherrera911@gmail.com>
2026-06-10 18:07:23 -0400

    feat: actualizamos la estrategia de cómo iobtener al usuario a través del token y mostrar información en el dashboard

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,7 +1,33 @@
-window.addEventListener("load", () => {
-    const usuario = JSON.parse(localStorage.getItem("sesionUsuario"));
+window.addEventListener("load", async () => {
+  try {
+    const token = localStorage.getItem("token");
 
-    document.getElementById("fullname").textContent = usuario.fullname;
-    document.getElementById("role").textContent = usuario.role;
-    document.getElementById("perfil_email").textContent = usuario.user;
-})
+    if (!token) {
+      window.location.href = "/";
+      return;
+    }
+
+    const respuesta = await fetch("http://localhost:3000/api/auth/me", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!respuesta.ok) {
+      throw new Error("No se pudo obtener la información del usuario");
+    }
+
+    const usuario = await respuesta.json();
+
+    document.getElementById("fullname").textContent = usuario.data.full_name;
+    document.getElementById("role").textContent = usuario.data.role;
+
+    document.getElementById("perfil_email").textContent = usuario.data.email;
+    document.getElementById("perfil_name").textContent = usuario.data.full_name;
+  } catch (error) {
+    console.error(error);
+
+    localStorage.removeItem("token");
+    window.location.href = "/login.html";
+  }
+});
