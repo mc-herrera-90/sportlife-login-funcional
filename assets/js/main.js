40ea207851df38410ab79ac1f00416ef618a57bf
mcherrera <mcherrera911@gmail.com>
2026-06-10 11:02:24 -0400

    chore(remove): remover comentarios de la implementación anterior

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,42 +51,12 @@ window.addEventListener("load", () => {
           feedback.classList.remove("d-none");
           feedback.textContent = "Rol no encontrado";
       }
+    
     } catch (error) {
       console.error(error);
       feedback.textContent = error.message;
       feedback.classList.remove("d-none");
     }
 
-    // Buscar al usuario
-    // const usuarioEncontrado = usuarios.find((usuario) => {
-    //     return usuario.user === correo.toLowerCase() && usuario.password === password.toLowerCase()
-    // })
-
-    // if (!usuarioEncontrado) {
-    //     feedback.classList.remove("d-none")
-    //     return;
-    // }
-
-    // Guardamos al usuario encontrado en el localStorage
-    // localStorage.setItem("sesionUsuario", JSON.stringify(usuarioEncontrado));
-
-    // Redirigir al dashboard según el rol
-    // switch(usuarioEncontrado.role) {
-    //     case "user":
-    //         window.location.href = "../dashboards/user.html";
-    //         break;
-
-    //     case "coach":
-    //         window.location.href = "../dashboards/coach.html";
-    //         break;
-
-    //     case "admin":
-    //         window.location.href = "../dashboards/admin.html";
-    //         break;
-
-    //     default:
-    //         feedback.classList.remove("d-none");
-    //         feedback.textContent = "Rol no encontrado";
-    // }
   });
 });
