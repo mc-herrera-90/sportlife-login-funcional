da41b2f447025c9de16756950f48ada1d412820d
mcherrera <mcherrera911@gmail.com>
2026-06-16 17:59:15 -0400

    avance en el registro

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", async () => {
   let usuariosSistema = [];
-  
+
   try {
     const token = localStorage.getItem("token");
     if (!token) {
@@ -26,19 +26,26 @@ window.addEventListener("load", async () => {
     document.getElementById("perfil_name").textContent = usuario.data.full_name;
 
     // LLenamos el campo de la seccion de perfil
-    document.getElementById("perfil-nombre-card").textContent = usuario.data.full_name;
+    document.getElementById("perfil-nombre-card").textContent =
+      usuario.data.full_name;
     document.getElementById("perfil-rol").textContent = usuario.data.role;
-    document.getElementById("perfil-email-card").textContent = usuario.data.email;
-    document.getElementById("perfil-fecha-card").textContent = usuario.data.birth_date;
+    document.getElementById("perfil-email-card").textContent =
+      usuario.data.email;
+    document.getElementById("perfil-fecha-card").textContent =
+      usuario.data.birth_date;
     document.getElementById("perfil-rol-card").textContent = usuario.data.role;
-    document.getElementById("perfil-registro").textContent = new Date(usuario.data.created_at).toLocaleDateString("es-CL");
+    document.getElementById("perfil-registro").textContent = new Date(
+      usuario.data.created_at,
+    ).toLocaleDateString("es-CL");
 
     // Formulario editable desde perfil
-    document.getElementById("fullname_perfil_input").value = usuario.data.full_name;
+    document.getElementById("fullname_perfil_input").value =
+      usuario.data.full_name;
     document.getElementById("email_perfil_input").value = usuario.data.email;
-    document.getElementById("fechaNacimiento_perfil_input").value = usuario.data.birth_date;
-    document.getElementById("deporteFavorito_perfil_input").value = usuario.data.metadata.sports?.[0]?.name || "";
-
+    document.getElementById("fechaNacimiento_perfil_input").value =
+      usuario.data.birth_date;
+    document.getElementById("deporteFavorito_perfil_input").value =
+      usuario.data.metadata.sports?.[0]?.name || "";
 
     if (usuario.data.role === "admin") {
       // Sólo cuando el login sea de un admin, realizamos la petición para obtener a los usuarios para gestionarlos
@@ -56,10 +63,16 @@ window.addEventListener("load", async () => {
 
       usuariosSistema = usuarios.data;
       const totalUsuarios = usuariosSistema.length;
-      const coaches = usuariosSistema.filter(usuario => usuario.role === 'coach');
+      const coaches = usuariosSistema.filter(
+        (usuario) => usuario.role === "coach",
+      );
+      const admins = usuariosSistema.filter(
+        (usuario) => usuario.role === "admin",
+      );
 
       document.getElementById("total_usuarios").textContent = totalUsuarios;
       document.getElementById("total_coaches").textContent = coaches.length;
+      document.getElementById("total_admin").textContent = admins.length;
 
       const tbody = document.getElementById("usuariosTableBody");
 
@@ -84,7 +97,7 @@ window.addEventListener("load", async () => {
               badgeRol = `<span class="badge text-bg-success">Usuario</span>`;
           }
 
-          return /*html*/`
+          return /*html*/ `
     <tr>
       <td>${usuario.id}</td>
       <td>
@@ -132,9 +145,9 @@ window.addEventListener("load", async () => {
   }
 
   // Listeners
-  const formNuevoUsuario = "";
+  const formNuevoUsuario = document.getElementById("formNuevoUsuario");
 
-  // Listener para el modal de editar
+  // Listener para llenar el modal de editar
   document.addEventListener("click", (e) => {
     const btnEditar = e.target.closest(".btn-editar");
 
@@ -152,8 +165,6 @@ window.addEventListener("load", async () => {
     document.getElementById("edit_email").value = usuario.email;
     document.getElementById("edit_birth_date").value = usuario.birth_date;
     document.getElementById("edit_role").value = usuario.role;
-    document.getElementById("edit_must_change_password").checked =
-      usuario.must_change_password;
     if (usuario.metadata?.sports?.length > 0) {
       document.getElementById("edit_sport").value =
         usuario.metadata.sports[0].name;
@@ -169,4 +180,211 @@ window.addEventListener("load", async () => {
 
     modalEditar.show();
   });
+
+  // Listener para eliminar
+  document.addEventListener("click", async (e) => {
+    const btnEliminar = e.target.closest(".btn-eliminar");
+    if (!btnEliminar) return;
+    const id = Number(btnEliminar.dataset.id);
+
+    const resultado = await Swal.fire({
+      title: "¿Eliminar usuario?",
+      text: "Esta acción no se puede deshacer.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Sí, eliminar",
+      cancelButtonText: "Cancelar",
+      reverseButtons: true,
+    });
+
+    if (!resultado.isConfirmed) return;
+    try {
+      const token = localStorage.getItem("token");
+
+      const respuesta = await fetch(`http://localhost:3000/api/users/${id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await respuesta.json();
+
+      if (!respuesta.ok) {
+        throw new Error(data.message);
+      }
+
+      await Swal.fire({
+        title: "Usuario eliminado",
+        text: "El usuario fue eliminado correctamente.",
+        icon: "success",
+        timer: 2000,
+        showConfirmButton: false,
+      });
+
+      // Eliminar de la lista local
+      usuariosSistema = usuariosSistema.filter((usuario) => usuario.id !== id);
+
+      // Eliminar la fila de la tabla sin recargar
+      btnEliminar.closest("tr").remove();
+      actualizarDashboard();
+    } catch (error) {
+      console.error(error);
+      Swal.fire({
+        title: "Error",
+        text: error.message,
+        icon: "error",
+      });
+    }
+  });
+
+  // Submit: Crear un usuario
+  formNuevoUsuario.addEventListener("submit", async (event) => {
+    let formularioValido = true;
+
+    event.preventDefault();
+
+    // Nodos del formulario
+    const nombre = event.target.elements.full_name;
+    const correo = event.target.elements.correo;
+    const nacimiento = event.target.elements.nacimiento;
+    const password1 = event.target.elements.password1;
+    const password2 = event.target.elements.password2;
+    const deporte = event.target.elements.deporte;
+    const frecuencia = event.target.elements.frecuencia;
+    const rol = event.target.elements.role_user;
+
+    // Validaciones de los campos
+    // Aquí solo validamos que el nombre tenga al menos 3 caracteres y además que no tenga números
+    const nombreInvalido =
+      nombre.value.trim().length < 3 || /\d/.test(nombre.value);
+
+    // Utilizamos toggle() para gestionar dinámicamente la clase "is-invalid"
+    nombre.classList.toggle("is-invalid", nombreInvalido);
+
+    if (nombreInvalido) {
+      formularioValido = false;
+    }
+
+    // Aquí podríamos usar una expresión regular para validar el correo, pero fuera de que quitamos la validación de HTML5.
+    // podemos apoyarnos del método checkValidity() de la API de validación de formularios del DOM de HTML5
+    const correoInvalido = !correo.checkValidity();
+
+    // Utilizamos toggle() para gestionar dinámicamente la clase "is-invalid"
+    correo.classList.toggle("is-invalid", correoInvalido);
+
+    if (correoInvalido) {
+      formularioValido = false;
+    }
+    // Y por último validamos que ambas contraseñas tengan una longitud mínima de 8 caracteres
+    const password1Valida = password1.value.trim().length >= 8;
+    const password2Valida = password2.value.trim().length >= 8;
+    // y que coincidan entre sí.
+    const passwordsCoinciden = password1.value === password2.value;
+
+    // Utilizamos toggle() para gestionar dinámicamente la clase "is-invalid"
+    password1.classList.toggle("is-invalid", !password1Valida);
+
+    password2.classList.toggle(
+      "is-invalid",
+      !password2Valida || !passwordsCoinciden,
+    );
+
+    if (!password1Valida || !password2Valida || !passwordsCoinciden) {
+      formularioValido = false;
+    }
+
+    if (!formularioValido) {
+      return;
+    }
+
+    const opcionfetch = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        full_name: nombre.value,
+        email: correo.value,
+        password: password1.value,
+        role: rol.value || "user",
+        birth_date: nacimiento.value,
+        metadata: {
+          sports: [
+            {
+              name: deporte.value,
+              frecuency_per_week: frecuencia.value || 0,
+            },
+          ],
+        },
+      }),
+    };
+    try {
+      const respuesta = await fetch(
+        `http://localhost:3000/api/auth/register`,
+        opcionfetch,
+      );
+
+      const data = await respuesta.json();
+
+      if (!respuesta.ok) {
+        console.log(data);
+        throw new Error(data.message);
+      }
+
+      // Una vez creado el usuario, reseteamos el formulario
+      event.target.reset();
+
+      await Swal.fire({
+        icon: "success",
+        title: "Usuario creado",
+        text: "El usuario fue registrado correctamente.",
+        confirmButtonText: "Aceptar",
+      });
+
+      // recargamos la página
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+
+      Swal.fire({
+        icon: "error",
+        title: "Error al crear usuario",
+        text: error.message,
+        confirmButtonText: "Aceptar",
+      });
+    }
+  });
+
+  // Actualizar Dashboard
+  function actualizarDashboard() {
+    const totalUsuarios = usuariosSistema.length;
+
+    const coaches = usuariosSistema.filter(
+      (usuario) => usuario.role === "coach",
+    );
+
+    const admins = usuariosSistema.filter(
+      (usuario) => usuario.role === "admin",
+    );
+
+    document.getElementById("total_usuarios").textContent = totalUsuarios;
+    document.getElementById("total_coaches").textContent = coaches.length;
+    document.getElementById("total_admin").textContent = admins.length;
+
+    const ultimosUsuarios = [...usuariosSistema]
+      .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+      .slice(0, 5);
+    document.getElementById("ultimosUsuariosBody").innerHTML = ultimosUsuarios
+      .map(
+        (usuario) => `
+      <tr>
+        <td>${usuario.full_name}</td>
+        <td>${usuario.role}</td>
+        <td>${new Date(usuario.created_at).toLocaleDateString("es-CL")}</td>
+      </tr>
+    `,
+      )
+      .join("");
+  }
+
+  actualizarDashboard();
 });
